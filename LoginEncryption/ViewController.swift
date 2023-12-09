183b2ec1aa9492b8548c8f18a214cf5c70ffac6b
keerthidevipriya <keerthidevipriya@gmail.com>
2023-12-09 20:38:46 +0530

    Basic Setup of the Login Encryption

---
+++ LoginEncryption/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginEncryption
+//
+//  Created by Keerthi Devipriya(kdp) on 08/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
