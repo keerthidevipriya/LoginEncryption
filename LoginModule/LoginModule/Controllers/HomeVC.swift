ca16a247a24f80e7532c2ca9532824e93a89af40
keerthidevipriya <keerthidevipriya@gmail.com>
2023-12-17 19:44:24 +0530

    Added the lock icon and details page UI Updated

--- LoginModule/LoginModule/Controllers/HomeVC.swift
+++ LoginModule/LoginModule/Controllers/HomeVC.swift
@@ -12,7 +12,10 @@ class HomeVC: UIViewController {
     var validations: LoginValidation?
     
     enum Constant {
-        static let margin: CGFloat = 16
+        static let margin: CGFloat = 48
+        static let iconMargin: CGFloat = 72
+        static let lockIcon = UIImage(named: "lockIcon")
+        static let bgImage = UIImage(named: "bluegradientbgImage") ?? UIImage()
     }
     
     lazy var baseView: UIView = {
@@ -21,11 +24,30 @@ class HomeVC: UIViewController {
         return view
     }()
     
+    lazy var lockImageView: UIImageView = {
+        var imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.image = Constant.lockIcon
+        imageView.center = view.center
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    lazy var bgImageView : UIImageView = {
+        var imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode = .scaleToFill
+        imageView.clipsToBounds = true
+        imageView.image = Constant.bgImage
+        imageView.center = view.center
+        return imageView
+    }()
+    
     lazy var titleLbl: UILabel = {
         var lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
         lbl.textColor = .black
-        lbl.font = UIFont.systemFont(ofSize: 14)
+        lbl.font = UIFont.systemFont(ofSize: 24)
         return lbl
     }()
     
@@ -33,7 +55,7 @@ class HomeVC: UIViewController {
         var lbl = UILabel()
         lbl.translatesAutoresizingMaskIntoConstraints = false
         lbl.textColor = .black
-        lbl.font = UIFont.systemFont(ofSize: 18)
+        lbl.font = UIFont.systemFont(ofSize: 20)
         return lbl
     }()
 
@@ -66,8 +88,10 @@ class HomeVC: UIViewController {
     }
     
     func configureView() {
+        baseView.addSubview(lockImageView)
         baseView.addSubview(titleLbl)
         baseView.addSubview(detailsLbl)
+        view.addSubview(bgImageView)
         view.addSubview(baseView)
     }
     
@@ -77,10 +101,15 @@ class HomeVC: UIViewController {
             baseView.rightAnchor.constraint(equalTo: self.view.rightAnchor),
             baseView.topAnchor.constraint(equalTo: self.view.topAnchor),
             baseView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-            baseView.heightAnchor.constraint(equalToConstant: 370),
+            baseView.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            
+            lockImageView.centerXAnchor.constraint(equalTo: baseView.centerXAnchor),
+            lockImageView.widthAnchor.constraint(equalToConstant: Constant.iconMargin),
+            lockImageView.heightAnchor.constraint(equalToConstant: Constant.iconMargin),
+            lockImageView.topAnchor.constraint(equalTo: baseView.topAnchor, constant: 370),
             
             titleLbl.centerXAnchor.constraint(equalTo: baseView.centerXAnchor),
-            titleLbl.topAnchor.constraint(equalTo: baseView.topAnchor, constant: 408),
+            titleLbl.topAnchor.constraint(equalTo: lockImageView.bottomAnchor, constant: Constant.margin),
             
             detailsLbl.centerXAnchor.constraint(equalTo: baseView.centerXAnchor),
             detailsLbl.topAnchor.constraint(equalTo: titleLbl.bottomAnchor, constant: Constant.margin)
@@ -88,7 +117,7 @@ class HomeVC: UIViewController {
     }
     
     func configureViewTheme() {
-        baseView.backgroundColor = .white
-        self.view.backgroundColor = .white
+        titleLbl.textColor = .white
+        detailsLbl.textColor = .white
     }
 }
